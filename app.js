d91c4f7f2f3216794cd6e14a38ea745d78ec9b7a
Adarsh Dhakar <adarshdhakar277@gmail.com>
2024-06-21 18:48:21 +0530

    listing model and initialize database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,7 +19,21 @@ async function main() {
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
-})
+});
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send(sampleListing);
+}) 
 
 app.listen(8080, () => {
     console.log("server is listening on port 8080");
---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String, 
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/a-beach-with-rocks-and-trees-Jg3vgxzhH4o",
+        set: (v) => v === "" ? "https://unsplash.com/photos/a-beach-with-rocks-and-trees-Jg3vgxzhH4o" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
