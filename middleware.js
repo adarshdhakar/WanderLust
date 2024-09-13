f8f0a8dcee516703320753696a519077642c50d5
Adarsh Dhakar <adarshdhakar277@gmail.com>
2024-09-13 17:53:10 +0530

    Doing

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const Listing = require("./models/listing.js");
 const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema} = require("./schema.js");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
@@ -52,12 +53,12 @@ module.exports.validateReview = ((req, res, next) => {
     }
 });
 
-module.exports.isReviewAuthor = async (req, res, next) => {
-    let {reviewId} = req.params;
+module.exports.isReviewAuthor = wrapAsync(async (req, res, next) => {
+    let {id} = req.params;
     let review = await Review.findById(id);
     if(!review.author._id.equals(res.locals.currUser._id)){
         req.flash("error", "You are not the author of this review!");
         return res.redirect(`/listings/${id}`);
     }
     next();
-};
+});
